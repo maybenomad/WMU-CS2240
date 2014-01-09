05714888ae770e87b666647c43b7aa6471d47f54
Rocco Nicosia <git@rocco.io>
2014-01-09 01:38:55 -0500

    Update map.h

--- A1_Map/map.h
+++ A1_Map/map.h
@@ -8,8 +8,13 @@ typedef struct _map
   map_entry_t* entry;  
 } map_t;
 
+// Part one functions.
 void map_init(map_t*);
 int map_put(map_t*, const char*, const char*);
 const char* map_get(map_t*, const char*);
 int map_remove(map_t*, const char*);
 void map_destroy(map_t*);
+
+// Part two functions. 
+int map_serialize(map_t* self, FILE* stream);
+int map_deserialize(map_t* self, FILE* stream);
