9d03ed96028c17ec51bab362107da2557f808b16
Rocco Nicosia <git@rocco.io>
2014-01-09 12:41:46 -0500

    Add asserts

--- A1_Map/map.c
+++ A1_Map/map.c
@@ -3,25 +3,31 @@ void map_init(map_t* self) {
 }
 
 int map_put(map_t* self, const char* key, const char* val) {
+  assert(self != NULL);
   
 }
 
 const char* map_get(map_t* self, const char* key) {
+  assert(self != NULL);
 
 }
 
 int map_remove(map_t* self, const char* key) {
+  assert(self != NULL);
   
 }
 
 int map_serialize(map_t* self, FILE* stream) {
+  assert(self != NULL);
   
 }
 
 int map_deserialize(map_t* self, FILE* stream) {
+  assert(self != NULL);
   
 }
 
 void map_destroy(map_t* self) {
+  assert(self != NULL);
   
 }
