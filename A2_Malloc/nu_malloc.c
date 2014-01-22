e01bffd0b73f061c1627dc69eb3a5ffc63bc4a44
Rocco Nicosia <git@rocco.io>
2014-01-22 09:58:51 -0500

    add nu_malloc files

---
+++ A2_Malloc/nu_malloc.c
@@ -0,0 +1,21 @@
+#include "nu_malloc.h"
+
+void* nu_malloc(size_t size)
+{
+  return NULL;  
+}
+
+void* nu_calloc(size_t count, size_t size) 
+{
+  return NULL;  
+}
+
+void* nu_realloc(void* ptr, size_t size) 
+{
+  return NULL; 
+}
+
+void nu_free(void* ptr)
+{
+  
+}
---
+++ A2_Malloc/nu_malloc.h
@@ -0,0 +1,7 @@
+// Included for the `size_t` type. 
+#include <string.h>
+
+void* nu_malloc(size_t);
+void* nu_calloc(size_t, size_t);
+void* nu_realloc(void*, size_t);
+void nu_free(void*);
